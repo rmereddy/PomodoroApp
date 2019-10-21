90f6235d899a471a33752f8974ae299248f3dce3
rohit <rohitmereddy4@gmail.com>
2019-10-20 22:43:18 -0400

    fix typo add screen shots and Updated README file

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PomodoroApp
-React Native Pomodoro app
+React Native Pomodoro app that allows users to select the time slot in mins and then schedule break. User is notified the end of focus session by a vibration sound
+
+#Run the app by running these commands
+1. run yarn - to install unmeet dependices
+2. Download Expo in IOS/Android Device
+3. run yarn start  
--- components/info.js
+++ components/info.js
@@ -6,7 +6,7 @@ export default class Info extends React.Component {
 	render() {
 	return (
 		<View style={styles.infoBox}>
-			<Text style={styles.infoText}>Reading about the effectivness of Pomodoro Techinique 
+			<Text style={styles.infoText}>Reading about the effectivness of Pomodoro Technique 
 				<Text style={{color: 'blue'}}
       				onPress={() => Linking.openURL('https://www.themuse.com/advice/take-it-from-someone-who-hates-productivity-hacksthe-pomodoro-technique-actually-works')}> here
       			</Text>.
