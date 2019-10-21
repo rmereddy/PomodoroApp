987b70140ef0b3de188e338b415717752be90a30
rohit <rohitmereddy4@gmail.com>
2019-10-20 22:21:16 -0400

    starter pomodoro clock

---
+++ components/info.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Linking, View, Text } from 'react-native';
+import { styles } from '../stylesheet';
+
+export default class Info extends React.Component {
+	render() {
+	return (
+		<View style={styles.infoBox}>
+			<Text style={styles.infoText}>Reading about the effectivness of Pomodoro Techinique 
+				<Text style={{color: 'blue'}}
+      				onPress={() => Linking.openURL('https://www.themuse.com/advice/take-it-from-someone-who-hates-productivity-hacksthe-pomodoro-technique-actually-works')}> here
+      			</Text>.
+			</Text>
+		</View>
+	)
+	}
+}
